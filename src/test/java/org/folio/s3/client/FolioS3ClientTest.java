fae127b87f5b751b4e38cc93a5b2cfbda543672d
Viachaslau Khandramai <Viachaslau_Khandramai@epam.com>
2024-04-11 12:42:27 +0200

    FOLS3CL-31 - large files upload

--- src/test/java/org/folio/s3/client/FolioS3ClientTest.java
+++ src/test/java/org/folio/s3/client/FolioS3ClientTest.java
@@ -89,7 +89,7 @@ class FolioS3ClientTest {
   @BeforeAll
   public static void setUp() {
 
-    DockerImageName localstackImage = DockerImageName.parse("localstack/localstack:3.3.0");
+    DockerImageName localstackImage = DockerImageName.parse("localstack/localstack:3.2.0");
 
     localstack = new LocalStackContainer(localstackImage)
             .withStartupTimeout(Duration.of(5, MINUTES))
